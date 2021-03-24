2cc43fa02f6c9194c42799f4cc8f65fa1dd16606
cheukmanli <46696600+cheukmanli@users.noreply.github.com>
2021-03-24 11:54:52 +0800

    added config.js

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export default {
+  api: {
+    baseUrl: "https://api.spotify.com/v1",
+    authUrl: "https://accounts.spotify.com/authorize",
+    clientId: "",
+    clientSecret: "",
+    clientUrl: "http://localhost:3000",
+  },
+};
